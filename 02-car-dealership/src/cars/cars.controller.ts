739b4f20ef952ded9b0c7c3050e26cbcfd894fb9
lucianopulido <lucianopulido1996@gmail.com>
2022-10-02 13:32:04 -0300

    DTOS y validacion de informacion2

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,39 +1,46 @@
-import {Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post} from '@nestjs/common';
-import {CarsService} from "./cars.service";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
-    constructor(
-        private readonly carsService: CarsService
-    ) {
-    }
+  constructor(private readonly carsService: CarsService) {}
 
-    @Get()
-    getAllCars() {
-        return this.carsService.getAllCars()
-    }
+  @Get()
+  getAllCars() {
+    return this.carsService.getAllCars();
+  }
 
-    @Get(':id')
-    getCarById(@Param('id', ParseIntPipe) id: number) {
-        return this.carsService.getCarById(id)
-    }
+  @Get(':id')
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.getCarById(id);
+  }
 
-    @Post()
-    createCar(@Body() body: any) {
-        return body
-    }
+  @Post()
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return this.carsService.create(createCarDto);
+  }
 
-    @Patch(':id')
-    updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
-        return body
-    }
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto,
+  ) {
+    return this.carsService.update(id, updateCarDto);
+  }
 
-    @Delete(':id')
-    deleteCar(@Param('id', ParseIntPipe) id: number) {
-        return {
-            method: 'delete',
-            id
-        }
-    }
-
+  @Delete(':id')
+  deleteCar(@Param('id',ParseUUIDPipe) id: string) {
+    return this.carsService.delete(id)
+  }
 }
--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -1,36 +1,66 @@
-import {Injectable, NotFoundException} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ICar } from './interfaces/cars.interface';
+import { v4 as uuid } from 'uuid';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Injectable()
 export class CarsService {
-    private cars = [
-        {
-            id: 1,
-            marca: 'Toyota',
-            modelo: 'Corolla'
-        },
-        {
-            id: 2,
-            marca: 'Honda',
-            modelo: 'Civic'
-        },
-        {
-            id: 3,
-            marca: 'Jeep',
-            modelo: 'Cherokee'
-        }
-    ]
+  private cars: ICar[] = [
+    {
+      id: uuid(),
+      marca: 'Toyota',
+      modelo: 'Corolla',
+    },
+    {
+      id: uuid(),
+      marca: 'Honda',
+      modelo: 'Civic',
+    },
+    {
+      id: uuid(),
+      marca: 'Jeep',
+      modelo: 'Cherokee',
+    },
+  ];
 
-    getAllCars() {
-        return this.cars
+  getAllCars() {
+    return this.cars;
+  }
+
+  getCarById(id: string) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) {
+      throw new NotFoundException(`Car With id ${id} not found`);
     }
 
-    getCarById(id: number) {
-        const car = this.cars.find(car => car.id === id)
+    return car;
+  }
+  create(createCarDto: CreateCarDto) {
+    const car: ICar = {
+      id: uuid(),
+      ...createCarDto, // esto es igual a hacer marca: createCarDto.marca , modelo: createCarDto.modelo
+    };
+    this.cars.push(car);
+    return car;
+  }
 
-        if (!car) {
-            throw new NotFoundException(`Car With id ${id} not found`)
-        }
+  update(id: string, updateCarDto: UpdateCarDto) {
+    let carDB = this.getCarById(id);
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        return (carDB = { ...carDB, ...updateCarDto, id });
+      }
+      return car;
+    });
 
-        return car
-    }
+    return carDB;
+  }
+
+  delete(id: string) {
+    const car = this.getCarById(id);
+    this.cars = this.cars.filter((car) => car.id !== id);
+    return car;
+  }
 }
